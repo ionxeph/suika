490d4268c7515fe8cf86b0940016fede30e36974
andrew-cheng <cheng0807@gmail.com>
2023-10-01 18:22:45 -0500

    add webpack just for copy? might come back to make it better

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './index.js',
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './index.html', to: './' },
+        { from: './pkg', to: 'pkg' },
+        { from: './assets', to: 'assets' },
+      ],
+    }),
+  ],
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true,
+  },
+};
